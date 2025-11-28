91b478fa7081622d4111d1ae0ba0c97dc12f8207
Akmal Ridho Rabbani <akmal.ridho.r@gmail.com>
2025-11-28 14:40:19 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert3_50422152/Pert3_50422152.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422152;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pert3_50422152 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
